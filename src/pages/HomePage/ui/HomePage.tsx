c0d8552511ac07a25e2d98b9c186e5a650db858b
Verhusha Oleg <olegverhusha@gmail.com>
2023-08-19 20:23:56 +0300

    add home page andfix loader

---
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface IHomePageProps {}
+
+const HomePage: FC<IHomePageProps> = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
---
+++ src/widgets/PageLoader/ui/PageLoader/PageLoader.tsx
@@ -0,0 +1,14 @@
+import { cn } from '@/shared/lib/classNames/classNames';
+import { Loader } from '@/shared/ui/Loader';
+
+import cls from './PageLoader.module.scss';
+
+interface PageLoaderProps {
+  className?: string;
+}
+
+export const PageLoader = ({ className }: PageLoaderProps) => (
+  <div className={cn(cls.PageLoader, {}, [className])}>
+    <Loader />
+  </div>
+);
